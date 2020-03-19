0f7dfa03cc1ea879a936568ad409bec2498c4721
Karthik Natarajan <karthik.natarajan@thoughtworks.com>
2020-03-19 17:07:03 +0530

    Value objects example

--- Day4/bookservice/valueobject/src/lengthvalueobject/Length.java
+++ Day4/bookservice/valueobject/src/lengthvalueobject/Length.java
@@ -1,4 +1,50 @@
 package lengthvalueobject;
 
-public class ValueObject {
+import java.util.Objects;
+
+public class Length {
+
+   private double value;
+   private String unit;
+
+    public Length(double value, String unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+
+        if(Objects.equals(unit, length.unit)){
+            return Objects.equals(value, length.value) &&
+                    Objects.equals(unit, length.unit);
+        }else{
+            if (length.unit.equals("cm")){
+                return value==convertToInch(length.value);
+            }else{
+                return convertToInch(value)==(length.value);
+            }
+        }
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, unit);
+    }
+
+    private double convertToInch(double cmValue){
+        return cmValue/2.5;
+    }
 }
--- Day4/bookservice/valueobject/src/lengthvalueobject/LengthTest.java
+++ Day4/bookservice/valueobject/src/lengthvalueobject/LengthTest.java
@@ -1,4 +1,41 @@
 package lengthvalueobject;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LengthTest {
+
+
+    @Test
+    public void testValueOfLength(){
+
+    Length length= new Length(1.0, "inch");
+    Length length1= new Length(1.0, "inch");
+
+    assertEquals(length, length1);
+    }
+
+    @Test
+    public void testValueConversionOfCmToInch(){
+        Length length= new Length(1.0, "inch");
+        Length length1= new Length(2.5, "cm");
+        assertEquals(length, length1);
+    }
+
+    @Test
+    public void testValueConversionOfInchToCm(){
+        Length length= new Length(2.5, "cm");
+        Length length1= new Length(1.0, "inch");
+        assertEquals(length, length1);
+    }
+
+    @Test
+    public void testValueConversionForHigherValues(){
+        Length length= new Length(7.5, "cm");
+        Length length1= new Length(3.0, "inch");
+        assertEquals(length, length1);
+    }
+
 }
+
